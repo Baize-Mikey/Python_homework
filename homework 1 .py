95ee1b76de6ba81509ede62b169d34a415f3afb1
Li Zheng Guo <113412175+Baize-Mikey@users.noreply.github.com>
2022-09-15 19:53:53 +0800

    homework 1
    学号：2020144141
    姓名：李正国
    作业次数：1

---
+++ homework 1 .py
@@ -0,0 +1,15 @@
+from turtle import *
+
+pendown()
+
+for i in range(6):
+    right(60)
+    forward(100)
+
+for i in range(6):
+    forward(100)
+    right(120)
+    forward(100)
+    left(60)
+
+done()
